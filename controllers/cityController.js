65c770a454c4926c89eeffaf86cc4591caca530c
Rasim <email@example.com>
2023-08-25 23:48:30 +0500

    .

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -42,33 +42,18 @@ class CityController {
 
     async create(req, res, next) {
         try{
-            // const {name} = req.body
+            const {name} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '../static', fileName))
+            img.mv(path.resolve(__dirname, '../static', 'cities', fileName))
 
-            // const city = await City.create({name, img: fileName})
-            return res.json(true)
+            const city = await City.create({name, img: fileName})
+            return res.json(city)
         }
         catch (e) {
             next(ApiError.badRequest(e.message))
         }
     }
-
-    // async create(req, res, next) {
-    //     try{
-    //         const {name} = req.body
-    //         const {img} = req.files
-    //         let fileName = uuid.v4() + ".jpg"
-    //         img.mv(path.resolve(__dirname, '../static', 'cities', fileName))
-    //
-    //         const city = await City.create({name, img: fileName})
-    //         return res.json(city)
-    //     }
-    //     catch (e) {
-    //         next(ApiError.badRequest(e.message))
-    //     }
-    // }
 }
 
 module.exports = new CityController()
